fabe896921df63f8e99e693e7e81db60af6e8469
Anton <antonsoldatov1983@gmail.com>
2022-03-31 23:11:30 +0200

    first commit

---
+++ scriptnumfile.js
@@ -0,0 +1,9 @@
+
+"use strict";
+
+let arr = [];
+arr[0] = prompt("question1","someData");
+arr[1] = prompt("question2","someData");
+arr[2] = prompt("question3","someData");
+
+document.write(arr);
