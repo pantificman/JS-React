f410488b9d2c9d6215047fcf3c555d51fdd6785f
Anton <antonsoldatov1983@gmail.com>
2022-04-04 22:25:52 +0200

    personalMovieDB

--- script (3).js
+++ script (3).js
@@ -1,6 +1,3 @@
-
-"use strict";
-
 /* Задание на урок:
 
 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
@@ -24,21 +21,5 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', "10");
-let personalMovieDB = {
-    count : numberOfFilms,
-    movies : {},
-    actors : {},
-    genres : [],
-    privat : false
-};
+'use strict';
 
- let a = prompt('Один из последних просмотренных фильмов?', ""),
-     b = prompt('На сколько оцените его?', ""),
-     c = prompt('Один из последних просмотренных фильмов?', ""),
-     d = prompt('На сколько оцените его?', "");
-
-     personalMovieDB.movies[a] = b;
-     personalMovieDB.movies[c] = d;
-
-     console.log(personalMovieDB);
